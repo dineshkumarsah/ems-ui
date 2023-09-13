a4e29dd7d6ed3e5117cbe29eeacfe30e2e651158
dinesh sah <dineshdqg@gamil.com>
2023-09-13 23:22:27 +0530

    header and employee api integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ems-ui';
+  constructor(private employeeService: EmployeeService){
+
+  }
+
+  getEmployeeList(){
+    this.employeeService.getAllEmployee().subscribe({
+      next: (res)=>{
+       console.log(res);
+       
+      },
+      error: (error)=>{
+         console.log(error);
+         
+      }
+    })
+
+  }
 }
